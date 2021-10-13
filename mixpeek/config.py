5c1bc0c3d098795e1d55465c1a53aa54181ff6d2
Ethan Steininger <esteininger21@gmail.com>
2021-10-13 16:34:51 -0700

    .

--- mixpeek/config.py
+++
@@ -1,7 +0,0 @@
-mixpeek_api_key = "IvHX2grjNZPQX7-pB0w7bzyUm34kFA4M3Cq8564uCiStZIvGfH-WiwASoXuSImIs7nw"
-
-aws = {
-    'aws_access_key_id': 'AKIASKFFMOJJVODSNVP2',
-    'aws_secret_access_key': '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',
-    'region_name': 'us-east-2'
-}
---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mixpeek',
+    version='1.0',
+    license='MIT',
+    author="Ethan Steininger",
+    author_email='info@mixpeek.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/mixpeek/mixpeek-python',
+    keywords='file search',
+    install_requires=['requests','boto3']
+)
